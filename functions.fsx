7ac9ebcf189e439fb0f0843559ef9ece2247e7ec
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-01-22 15:21:59 +0100

    Add functions

---
+++ functions.fsx
@@ -0,0 +1,20 @@
+// Simple function
+let add x y = x + y
+add 1 2
+
+let square x = x * x
+let twoSquared = square 2
+printfn "2 squared is %i" twoSquared
+
+// Recursive function
+let rec pow x y = 
+    if y = 0
+    then 1
+    else x * pow x (y-1)
+
+pow 10 3
+
+let cube x =
+    pow x 3
+
+cube 2
