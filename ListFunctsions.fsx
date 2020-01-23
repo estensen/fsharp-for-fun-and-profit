59f40594ca86119ce60cba059aaa76f19398159d
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-01-23 09:46:59 +0100

    Add list functions

---
+++ ListFunctsions.fsx
@@ -0,0 +1,30 @@
+// Basic list manipulation
+let oneToFive = [1;2;3;4;5]
+let zeroToFive = 0 :: oneToFive
+let zeroToSeven = zeroToFive @ [6;7]
+
+
+let rec sumList =
+    match xs with
+    | [] -> 0
+    | head :: tail -> head + sumList tail
+
+sumList [1;2;3;4]
+
+
+let rec prodList (xs : int list) =
+    match xs with
+    | [] -> 1
+    | head :: tail -> head * prodList tail
+
+prodList [1;2;3;4]
+
+
+let square x = x * x
+
+let sumOfSquares n =
+    [1..n]
+    |> List.map square
+    |> List.sum
+
+sumOfSquares 100
