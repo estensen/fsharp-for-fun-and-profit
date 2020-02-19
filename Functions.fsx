38f43ed61e781a2ea3af859cf6ee922c12dcc2fc
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-02-19 19:40:37 +0100

    Add add1

--- Functions.fsx
+++ Functions.fsx
@@ -11,10 +11,15 @@ let rec pow x y =
     if y = 0
     then 1
     else x * pow x (y-1)
-
 pow 10 3
 
 let cube x =
     pow x 3
-
 cube 2
+
+
+// Every multi parameter function can be converted into
+// a on parameter function
+let add1 = (+) 1
+add1 2
+
